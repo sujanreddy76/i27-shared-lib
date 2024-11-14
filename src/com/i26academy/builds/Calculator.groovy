bf052c62acbc49c6144bd1bfdff516a24f622f13
sujanreddy76 <sujan.reddy.76@gmail.com>
2024-11-14 21:49:54 +0530

    added shared library

---
+++ src/com/i26academy/builds/Calculator.groovy
@@ -0,0 +1,24 @@
+//The below line indicates that we are declaring package for builds folder, whenever we use the methods from builds package we have to import this package
+package com.i27academy.builds;
+
+//Defining a class
+class Calculator(){
+    //declaring a variable (We use this variable for builds deployment)
+    def jenkins
+    //constructor(it will take reference for calling jenkins,running scripts it will take reference of jenkins variable)
+    //jenkins variable will hold jenkins reference here
+    calculator(jenkins){
+
+    }
+    def add(firstNumber, secondNumber) {
+        //logical code base here
+        return firstNumber + secondNumber
+    }
+    def multiply(firstNumber, secondNumber) {
+        //logical code base here
+        return firstNumber * secondNumber
+    }
+
+}
+
+
---
+++ vars/firstPipeline.groovy
@@ -0,0 +1,61 @@
+import com.i27academy.builds.Calculator
+
+def call(Map pipelineParams){
+    //Object/instance of the calculator class
+    Calculator calculator=new Calculator(this)
+  pipeline {
+    agent {
+        label 'java-label'
+    }
+    environment {
+        APPLICATION_NAME = "${pipelineParams.appName}"
+    }
+    stages {
+        stage('Calculate'){
+            steps{
+                script{
+                    echo "Calling calculator method from src folder"
+                    echo "*********Printing the sum of values ****************"
+                    calculator.add(2,3)
+                }
+            }
+
+        }
+        stage('Build') {
+            steps {
+                echo "Building the project"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "Testing the project"
+            }
+        }
+        stage('DevDeploy') {
+            steps {
+                echo "Deploying the project to Dev environment"
+            }
+        }
+         stage('TestDeploy') {
+            steps {
+                echo "Deploying the project to Test environment"
+            }
+        }
+         stage('ProdDeploy') {
+            steps {
+                echo "Deploying the project to Prod environment"
+            }
+        }
+    }
+  }
+}
+
+def cal(firstNumber,secondNumber){
+    return firstNumber+secondNumber
+}
+
+//${variable} //If we defined normal variable then we call this way
+//${env.VARIABLE} //If we defined environment variable then we call this way
+//${parms.VARIABLE} //If we defined parameters then we call this way
+//${pipelineParams.VARIABLE} //If we implement shared library the if one project has to call variable from another project then we will use 'pipelineParams'
+
